8af4c61813b78f68e64909ad75884da3310c7a40
DESKTOP-RD3V04C\molaj <jakubmolek2000@gmail.com>
2022-03-20 20:21:21 +0100

    CreateCommand method logic updates

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,30 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            if (parts[2] == "standard")
+            {
+                BaseGradeBook gradeBook = new BaseGradeBook(name);
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradeBook);
+            }
+            else if (parts[2] == "ranked")
+            {
+                BaseGradeBook RankedGradeBook = new RankedGradeBook(name);
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(RankedGradeBook);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not supported type of gradebook, please try again", parts[2]);
+                return;
+            }
+        
         }
 
         public static void LoadCommand(string command)
